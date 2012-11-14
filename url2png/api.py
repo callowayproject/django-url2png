1401611b08284f7d247fc11f89f2fd8b2513fe6d
Corey Oordt <coreyoordt@gmail.com>
2012-11-14 11:58:23 -0600

    Version 0.4: Removed cruft files and added some utility functions for downloading images

--- url2png/api.py
+++ url2png/api.py
@@ -1,15 +1,26 @@
 import hashlib
+import urllib
+try:
+    from cStringIO import StringIO
+except ImportError:
+    from StringIO import StringIO
+try:
+    import Image
+except ImportError:
+    from PIL import Image
+
+from django.core.files.base import ContentFile
 
 from url2png.settings import (API_KEY, SECRET_KEY, API_URL, DEFAULT_VIEWPORT,
                               DEFAULT_FULLPAGE, DEFAULT_THUMBNAIL_MAX_WIDTH,
                               THUMBNAIL_MAX_WIDTH, VIEWPORT, FULLPAGE)
+
 
 def non_default_options(url, viewport=DEFAULT_VIEWPORT, fullpage=DEFAULT_FULLPAGE,
                         thumbnail_max_width=DEFAULT_THUMBNAIL_MAX_WIDTH):
     """
     Returns a Query String with only non-default options.
     """
-    import urllib
     output = {'url': url}
     if viewport != DEFAULT_VIEWPORT:
         output['viewport'] = viewport
@@ -36,3 +47,54 @@ def make_api_url(url, viewport=VIEWPORT, fullpage=FULLPAGE,
     result = '/'.join((API_URL, API_KEY, token, 'png/?%s' % options))
 
     return result
+
+
+def download_website_image(url, filepath=None, viewport=VIEWPORT,
+                           fullpage=FULLPAGE,
+                           thumbnail_max_width=THUMBNAIL_MAX_WIDTH):
+    """
+    Given the url of a webpage, download the thumbnail of the website.
+
+    If filepath is given, image is downloaded into that path, otherwise image
+    is saved into a temporary file.
+
+    Returns the filepath to the saved image
+    """
+    api_params = {
+        'url': url,
+        'viewport': viewport,
+        'fullpage': fullpage,
+        'thumbnail_max_width': thumbnail_max_width,
+    }
+    info = urllib.urlretrieve(make_api_url(**api_params), filepath)
+    return info[0]
+
+
+def django_website_image(url, viewport=VIEWPORT, fullpage=FULLPAGE,
+                         thumbnail_max_width=THUMBNAIL_MAX_WIDTH):
+    """
+    Returns a filename, (width, height), ContentFile tuple for saving in a field
+
+    the filename is a hash of the parameters passed to the function to avoid
+    naming collisions
+
+    filename, dimensions, contentfile = django_website_image(self.url)
+    self.key_image.save(filename, contentfile)
+    """
+    api_params = {
+        'url': url,
+        'viewport': viewport,
+        'fullpage': fullpage,
+        'thumbnail_max_width': thumbnail_max_width,
+    }
+    filename = "%s.png" % hashlib.md5(''.join(map(str, api_params.values()))).hexdigest()
+    filepath = download_website_image(**api_params)
+    image = Image.open(filepath)
+    if image.mode not in ('L', 'RGB'):
+        image = image.convert('RGB')
+
+    destination = StringIO()
+    image.save(destination, format='PNG')
+    destination.seek(0)
+
+    return (filename, image.size, ContentFile(destination.read()))
